debfc8f3f3af263e72464f2fc24f529ddc65ecdc
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-03-07 13:09:37 +0530

    FileCheckerAndEmailNotifier.java

--- src/test/java/com/test/PP_Machines_storage/FileCheckerAndEmailNotifier.java
+++ src/test/java/com/test/PP_Machines_storage/FileCheckerAndEmailNotifier.java
@@ -124,9 +124,10 @@ public class FileCheckerAndEmailNotifier {
 
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.ssl.enable", "true");
         properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
         javax.mail.Session mailSession = javax.mail.Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
